d7c57554cae27376d7a212a2d556d65876d3cc96
Christian Marin <chrismarin0607@gmail.com>
2025-08-21 19:30:17 -0400

    feat: add new navbar and new styles for project

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,9 +2,9 @@ import Hero from "@/components/layout/Hero";
 import type React from "react";
 
 export default function HomePage(): React.JSX.Element {
-    return (
-        <>
-            <Hero />
-        </>
-    );
+  return (
+    <>
+      <Hero />
+    </>
+  );
 }
